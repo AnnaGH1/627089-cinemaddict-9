bf915fd8b31a0cc2c4729a68c9c82a67ea57a20a
Anna Bekkarevich <annabekkarevich@gmail.com>
2019-09-21 15:37:31 -0400

    feat: Update data on control click (popup)

--- src/controllers/film.js
+++ src/controllers/film.js
@@ -44,6 +44,33 @@ export default class FilmController {
     this._popup.getElement()
       .querySelector(`.film-details__comment-input`)
       .addEventListener(`blur`, () => document.addEventListener(`keydown`, onEscKeyDown));
+
+    // Toggle watchlist
+    this._popup.getElement()
+      .addEventListener(`click`, (e) => {
+        if (e.target.classList.contains(`film-details__control-label--watchlist`)) {
+          e.preventDefault();
+          this._onPopupControlClick(e);
+        }
+      });
+
+    // Toggle history
+    this._popup.getElement()
+      .addEventListener(`click`, (e) => {
+        if (e.target.classList.contains(`film-details__control-label--watched`)) {
+          e.preventDefault();
+          this._onPopupControlClick(e);
+        }
+      });
+
+    // Toggle favorites
+    this._popup.getElement()
+      .addEventListener(`click`, (e) => {
+        if (e.target.classList.contains(`film-details__control-label--favorite`)) {
+          e.preventDefault();
+          this._onPopupControlClick(e);
+        }
+      });
   }
 
   init() {
@@ -56,5 +83,30 @@ export default class FilmController {
       });
 
     render(this._container, this._film.getElement(), Position.BEFOREEND);
+  }
+
+  _onPopupControlClick(e) {
+    e.target.previousElementSibling.toggleAttribute(`checked`);
+    const formData = new FormData(this._popup.getElement().querySelector(`.film-details__inner`));
+    const entry = {
+      title: this._data.title,
+      category: this._data.category,
+      rating: this._data.rating,
+      year: this._data.year,
+      duration: this._data.duration,
+      country: this._data.country,
+      director: this._data.director,
+      writers: this._data.writers,
+      actors: this._data.actors,
+      genres: this._data.genres,
+      url: this._data.url,
+      description: this._data.description,
+      comments: this._data.comments,
+      isWatchlist: !!formData.get(`watchlist`),
+      isHistory: !!formData.get(`watched`),
+      isFavorites: !!formData.get(`favorite`),
+    };
+
+    this._onDataChange(entry, this._data);
   }
 }
